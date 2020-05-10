463ac35c49b626a4d790d12fc4e2c05fc3ea8000
Jose Ortega <joseortegaaula@gmail.com>
2020-05-10 17:50:55 +0200

    Jose - Update Routes
    Add Booking Route to template

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('service', 'ServiceController');
+Route::resource('service', 'ServiceController')->middleware('auth');
+Route::resource('booking', 'BookingController')->middleware('auth');
