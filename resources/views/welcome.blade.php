0ae03660d4f465b9a2e85d15738e8f32cff4c73c
unknown <khidalgo19.kh@gmail.com>
2020-05-11 14:55:24 +0200

    Kevin/cambios del Git local al git remote

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Kan Cruz</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kan Cruz
                 </div>
 
                 <div class="links">
@@ -93,6 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">Contact Us</a>
+
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::resource('service', 'ServiceController');
 Route::resource('room', 'RoomController');
 Route::resource('booking', 'BookingController');
