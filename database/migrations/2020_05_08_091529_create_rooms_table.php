fbf7861033b72723565e4e44826c203007dc6268
Sofia Carballo <sofia.carballo@live.com>
2020-05-12 09:08:46 +0200

    Sofia -- fixed ID field

--- database/migrations/2020_05_08_091529_create_rooms_table.php
+++ database/migrations/2020_05_08_091529_create_rooms_table.php
@@ -14,7 +14,7 @@ class CreateRoomsTable extends Migration
     public function up()
     {
         Schema::create('rooms', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->integer('cost')->nullable();
             $table->string('status')->default();
--- database/migrations/2020_05_08_091732_create_contacts_table.php
+++ database/migrations/2020_05_08_091732_create_contacts_table.php
@@ -14,7 +14,7 @@ class CreateContactsTable extends Migration
     public function up()
     {
         Schema::create('contacts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->longText('description');
