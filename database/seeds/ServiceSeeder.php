0f265f09994837385b9a7afba31f6a6750b3c22e
Sofia Carballo <sofia.carballo@live.com>
2020-05-13 11:17:23 +0200

    Sofia ---  Created seeders

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -1,16 +1,13 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Service;
 
 class ServiceSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        //
+        factory(Service::class,2)->create();
     }
 }
