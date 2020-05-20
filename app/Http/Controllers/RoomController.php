26b527c1dad0fb117093f9598bd80248eae61a3c
Sofia Carballo <sofia.carballo@live.com>
2020-05-20 18:38:00 +0200

    Sofia --- deleted comments

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -8,80 +8,39 @@ use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $rooms = Room::all();
         return view('room.index',['rooms'=>$rooms]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('room.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         Room::create($request->all());
         return redirect(Route('room.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
-     */
     public function show(Room $room)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Room $room)
     {
         return view('room.edit',['room'=>$room]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Room $room)
     {
         $room->update($request->all());
         return redirect(route('room.index'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Room $room)
     {
         $room->delete();
