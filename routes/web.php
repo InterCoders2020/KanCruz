ebad2483c693aa92f556b34adac4ea98b25a45cc
Sofia Carballo <sofia.carballo@live.com>
2020-05-20 18:34:27 +0200

    Sofia --- add routes

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('service', 'ServiceController')->middleware('auth');
 Route::resource('room', 'RoomController')->middleware('auth');
 Route::resource('booking', 'BookingController')->middleware('auth');
-Route::resource('contact', 'ContactController');
+Route::resource('contact', 'ContactController')->middleware('auth');
+Route::resource('calendar', 'CalendarController')->middleware('auth');
+Route::resource('availability', 'AvailabilityController')->middleware('auth');
