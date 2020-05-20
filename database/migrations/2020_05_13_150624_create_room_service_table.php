b030a0066704ea4137ca26d41fbf9a4ae2000502
Sofia Carballo <sofia.carballo@live.com>
2020-05-20 18:40:59 +0200

    Sofia --- create pivot table

---
+++ database/migrations/2020_05_13_150624_create_room_service_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room_service', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->foreignId('service_id');
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room_service');
+    }
+}
