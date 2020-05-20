dd921554bfe513775d37d79ea185ce6b12ac672e
Sofia Carballo <sofia.carballo@live.com>
2020-05-20 18:41:23 +0200

    Sofia --- Add foreign key column

---
+++ database/migrations/2020_05_19_121515_add_room_to_bookings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoomToBookings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->foreignId('room_id')->nullable();
+            $table->foreign('room_id')->references('id')->on('rooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+        });
+    }
+}
