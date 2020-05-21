a904d86e6eb4bf3361dc43e37c0d1006fe7bf9c1
Sofia Carballo <sofia.carballo@live.com>
2020-05-21 23:37:30 +0200

    Sofia --- Add map Contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -36,7 +36,9 @@
 
         </div>
 
-        <div class="col-md-6" id="map"></div>
+        <div class="col-md-6" id="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2978.1272506417317!2d2.284077915678027!3d41.717772883410596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4c5781a8b6bcb%3A0x6dd4d1b5896df742!2sMasia%20Can%20Plans!5e0!3m2!1sen!2sus!4v1590096781453!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
       </div>
     </div>
   </div>
