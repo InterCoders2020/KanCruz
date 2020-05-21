7bd895543c39473f43229b456a84e152f959251d
Sofia Carballo <sofia.carballo@live.com>
2020-05-21 08:11:21 +0200

    Sofia --- Add footer section

---
+++ resources/views/layouts/includes/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+    <div class="container">
+      <div class="row mb-5">
+
+
+        <div class="col-md-6 col-lg-4">
+          <div class="block-23">
+            <h3 class="heading-section">Contact Info</h3>
+              <ul>
+                <li><a href='#'><span class="icon icon-map-marker"></span><span class="text">Masia KanCruz, 08590, Figaró-Montmany, Catalunya, Spain</span></a></li>
+                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+34 607304498</span></a></li>
+                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">masiacanplans.montseny@gmail.com</span></a></li>
+              </ul>
+            </div>
+        </div>
+
+
+      </div>
+      <div class="row pt-5">
+        <div class="col-md-12 text-left">
+          <p>
+            InterCoders &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
